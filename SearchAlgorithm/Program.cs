0349c30d41cad3924cc92399dba19e5db4b98fbd
Anisa Fauziah <anisa.fauziah.ft21@mail.umy.ac.id>
2022-10-24 16:18:34 +0700

    Add project files.

---
+++ SearchAlgorithm/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchAlgorithm
+{
+    class Program
+    {
+        //Array to be searched
+        int[] arr = new int[20];
+        //Number of elements in the array
+        int n;
+        //Get the number of elements to store in the array
+        int i;
+        public void input()
+        {
+            while (true)
+            {
+                Console.Write("Enter the number of the elements in the array: ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 20))
+                    break;
+                else
+                    Console.WriteLine("|rArray should have minimum 1 and maximum 20 elements.\n");
+            }
+        }
+    }
+}
