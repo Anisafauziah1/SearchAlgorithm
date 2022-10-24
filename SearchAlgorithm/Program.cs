10f685f841f188ddb10e4778143417d9d1ee1598
Anisa Fauziah <anisa.fauziah.ft21@mail.umy.ac.id>
2022-10-24 16:50:54 +0700

    loop to search for the elements in the array

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -54,7 +54,29 @@ namespace SearchAlgorithm
                 // obtain the index of the middle elements
                 int mid = (lowerbound + upperbound) / 2;
                 int ctr = 1;
-            }
+
+                //loop to search for the elements in the array
+                while ((item != arr[mid]) && (lowerbound <= upperbound))
+                {
+                    if (item > arr[mid])
+                        lowerbound = mid + 1;
+                    else
+                        upperbound = mid - 1;
+
+                    mid = (lowerbound + upperbound) / 2;
+                    ctr++;
+                }
+                if (item == arr[mid])
+                    Console.WriteLine("\n" + item.ToString() + " found at position " + (mid + 1).ToString());
+                else
+                    Console.WriteLine("\n" + item.ToString() + " Not found in the array\n");
+                Console.WriteLine("\nNumber of comparasion : " + ctr);
+
+                Console.Write("\nContinue search (y/n):");
+                ch = char.Parse(Console.ReadLine());
+
+            } while ((ch == 'y') || (ch = 'Y'));
         }
+        
     }
 }
